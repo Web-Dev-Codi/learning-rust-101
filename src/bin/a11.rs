72f735b9ab61003efa10395552752ddccd06c3ad
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-08-28 11:24:51 +0200

    Ownership

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -9,4 +9,23 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem {
+    quantity: i32,
+    id: i32,
+}
+// Using barrowing of the GroceryItem
+fn display_quantity(grocery_item: &GroceryItem) {
+    println!("Quantity: {:?}", grocery_item.quantity)
+}
+// Using barrowing of the GroceryItem
+fn display_id(grocery_item: &GroceryItem) {
+    println!("ID: {:?}", grocery_item.id)
+}
+
+fn main() {
+    // Creating an instance of the GroceryItem struct
+    let meal = GroceryItem { quantity: 5, id: 9 };
+    // Using the ampersand to initialize barrowing
+    display_quantity(&meal);
+    display_id(&meal)
+}
