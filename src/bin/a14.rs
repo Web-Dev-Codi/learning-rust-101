dc68b2e521c580e88785918f2996aca4f15309c7
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-08-30 18:51:37 +0200

    Test

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,46 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String,
+}
+
+// Barrowing name and fav_color here to use in our if expression
+fn print_info(name: &str, fav_color: &str) {
+    println!("Name: {:?} Favorite Color: {:?}", name, fav_color);
+}
+
+fn main() {
+    let persons = vec![
+        Person {
+            age: 1,
+            name: "Astra".to_owned(), //  using to_owned() for ownership
+            fav_color: "Red".to_owned(),
+        },
+        Person {
+            age: 40,
+            name: "Brian".to_owned(),
+            fav_color: "Black".to_owned(),
+        },
+        Person {
+            age: 35,
+            name: "Gülden".to_owned(),
+            fav_color: "Purple".to_owned(),
+        },
+        Person {
+            age: 1,
+            name: "Neby".to_owned(),
+            fav_color: "Green".to_owned(),
+        },
+    ];
+
+    for i in persons {
+        if i.age <= 10 {
+            // Barrowed name AND fav_color here
+            print_info(&i.name, &i.fav_color);
+            println!("{:?} you are a young rugrat", i.name);
+        }
+    }
+}
